1e26cbcbec4afee2606fd1e426202742fa3f4d4a
Sean Staz <seanstaz52@gmail.com>
2014-12-22 21:32:38 -0600

    Create Shape.java

---
+++ Shape.java
@@ -0,0 +1,18 @@
+//Sean Staz
+//CSci 112
+//Java II
+
+public class Shape 
+{
+	private double area;
+	
+	public void setArea(double a)
+	{
+		area = a;
+	}
+	
+	public double getArea()
+	{
+		return area;
+	}
+}
