9ea8da12e9953a0bf9857ccab483ea0e006c7ceb
Sean Staz <seanstaz52@gmail.com>
2014-12-22 21:34:21 -0600

    Create Square.java

---
+++ Square.java
@@ -0,0 +1,16 @@
+//Sean Staz
+//CSci 112
+//Java II
+
+public class Square extends Shape
+{
+	private double height;
+	private double width;
+	
+	public Square(double h, double w)
+	{
+		height = h;
+		width = w;
+		setArea(height * width);
+	}
+}
